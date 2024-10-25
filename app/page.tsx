bbe209b8938f25c7424c372cf017ef9d9b4ecaed
Megan Nguyen <nguyenmegan@berkeley.edu>
2024-10-25 10:18:08 -0400

    delete home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Welcome } from '../components/Welcome/Welcome';
+import { TableSort } from '../components/TableSort/TableSort';
+import { HeaderSimple } from '../components/HeaderSimple/HeaderSimple';
+import { Container } from '@mantine/core';
+import { Title, Text } from '@mantine/core';
 
-export default function HomePage() {
-  return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
-  );
+export default function Explorer() {
+    return (
+        <>
+            <HeaderSimple />
+            <Container px={0} size="80rem">
+                <Title ta="center" mt={100}>
+                    <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
+                        Data Explorer
+                    </Text>
+                </Title>
+                <TableSort />
+            </Container>
+        </>
+    );
 }
